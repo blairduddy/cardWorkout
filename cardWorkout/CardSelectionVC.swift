02de463938495e685c33a67b9455e1f099e52946
Blair Duddy <blairduddy@hotmail.co.uk>
2023-04-08 22:04:09 -0400

    Addition of basic UI - no logic.

---
+++ cardWorkout/CardSelectionVC.swift
@@ -0,0 +1,29 @@
+//
+//  CardSelectionVC.swift
+//  cardWorkout
+//
+//  Created by Blair Duddy on 2023-04-08.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
